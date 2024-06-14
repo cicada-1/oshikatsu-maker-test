65c40886b7990f282ee665eb1065cd4a0b15b433
Peter Wallace <peterwallace@hotmail.co.uk>
2024-06-14 14:07:06 +0900

    made submit button larger

--- app/page.tsx
+++ app/page.tsx
@@ -173,10 +173,10 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="mt-6 flex items-center gap-x-6">
+        <div className="mt-6 flex flex-col items-center gap-x-6">
           <button
             type="submit"
-            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-md bg-indigo-600 px-14 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             創る
           </button>
