5c91ad05ad582d3439f50735071c06fbb2fac6dc
Peter Wallace <peterwallace@hotmail.co.uk>
2024-06-17 10:39:05 +0900

    Added placeholder to age field

--- app/page.tsx
+++ app/page.tsx
@@ -193,6 +193,7 @@ export default function Home() {
                     id="age"
                     value={formData.age}
                     max={120}
+                    placeholder="40"
                     className="block flex-1 rounded-md border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     onInvalid={(e) => { e.target.setCustomValidity("120まで入力できます") }}
                     onChange={ageChangeHandler}
