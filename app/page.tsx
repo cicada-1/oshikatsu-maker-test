3c3861e79624c11aa28e902765025154fbc3d9f7
Peter Wallace <peterwallace@hotmail.co.uk>
2024-06-20 10:04:22 +0900

    Added margin to bottom of download button

--- app/page.tsx
+++ app/page.tsx
@@ -286,7 +286,7 @@ export default function Home(this: any) {
                 </div>
                 <button
                   id="download-button"
-                  className="download-button rounded-md bg-indigo-600 mt-10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="download-button rounded-md bg-indigo-600 my-10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   onClick={screenshotDownload}
                 >
                   保存する
