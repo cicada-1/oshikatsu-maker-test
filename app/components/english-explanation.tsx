abab918d6d5e4732f3be7f3c8223e6751853e19e
Peter Wallace <peterwallace@hotmail.co.uk>
2024-06-21 10:53:55 +0900

    Translated the explanation to English

--- app/components/english-explanation.tsx
+++ app/components/english-explanation.tsx
@@ -6,19 +6,19 @@ export default function EnglishExplanation(this: any) {
 
   return (
     <div className="heading m-5 flex flex-col items-center">
-      <h1 className="form-title mb-5 font-bold leading-7 text-gray-900">私の「推し勝★」ジェネレーター</h1>
+      <h1 className="form-title mb-5 font-bold leading-7 text-gray-900">My &quot;Oshi-Katsu&quot; Generator!</h1>
       <p
         className="explanation"
         style={{ whiteSpace: 'pre-wrap' }}
       >
-        自分の「推し勝★」を創ってみましょう！<br /> <br />
-        私の「推し勝★」ジェネレーターに：<br /> <br />
-        -「推し勝★」<br />
-        - 創る未来<br />
-        - ペンネーム<br />
-        - 年齢<br />
-        - 写真<br /> <br />
-        を入力して、キャンペーンのポスターのように自分のができます！
+        Create your own &quot;Oshi-Katsu&quot; poster!<br /> <br />
+        Please enter:<br /> <br />
+        - Your &quot;Oshi-Katsu&quot;<br />
+        - Your belief for the future<br />
+        - Nickname<br />
+        - Age<br />
+        - A picture<br /> <br />
+        to the generator and you can make your own poster, just like those of the official campaign!
       </p>
       <div className="my-5 flex flex-col items-center">
         <Image
@@ -28,12 +28,12 @@ export default function EnglishExplanation(this: any) {
           alt="not found"
           src={posterExample.src}
         />
-        <p className="image-note"><em>ポスターの例</em></p>
+        <p className="image-note"><em>Example poster</em></p>
       </div>
       <p className="explanation">
-        このように完成すると、保存できますので、完成したポスターか、「保存する」のボタンをクリックしてください！<br />
-        ポスターを保存してから、「#私の推し勝」「#勝山しか勝たん山」のハッシュタグでSNSに投稿しましょう！<br />
-        では、私の「推し勝★」ジェネレーターで楽しみましょう！
+        Once your poster has been generated, click on the generated poster or the &quot;download&quot; button below to download your poster.<br />
+        If you upload your poster to social media, remeber to use the hashtags #私の推し勝, #勝山しか勝たん山. Or in English #MyOshiKatsu, #KatsuyamaShikaKatanYama.<br />
+        Have fun with the My &quot;Oshi-Katsu&quot; Generator!
       </p>
     </div>
   );
