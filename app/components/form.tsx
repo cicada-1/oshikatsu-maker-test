bed3e80815cd55d661656d44e8e840a7c24ed463
Peter Wallace <peterwallace@hotmail.co.uk>
2024-06-26 16:01:45 +0900

    Moved poster output back to form component

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Image from 'next/image';
-import PosterOutput from './poster-output';
+import * as htmlToImage from 'html-to-image';
+import template from '../assets/images/oshikatsu-template.jpg';
 
-export default function Form() {
+export default function Form(this: any) {
 
   const [formData, setFormData] = useState({
     oshikatsu: '',
@@ -58,6 +59,18 @@ export default function Form() {
       [e.target.age.name]: e.target.age.value,
     });
     setTimeout(autoScroll, 500);
+  };
+
+  const screenshotRef = useRef(this);
+
+  function screenshotDownload() {
+    htmlToImage.toJpeg(screenshotRef.current, { quality: 0.95 })
+      .then(function (dataUrl) {
+        var link = document.createElement('a');
+        link.download = 'watashi-no-oshikatsu.jpeg';
+        link.href = dataUrl;
+        link.click();
+      });
   };
 
   return (
@@ -196,10 +209,48 @@ export default function Form() {
         </div>
       </form>
 
-      <PosterOutput
-        submittedText={submittedText}
-        submittedImage={submittedImage}
-      />
+      <div className="result mt-2">
+        {submittedImage && (
+          <div id="poster">
+            <div id="template" className="template flex flex-col items-center max-w-5xl p-5">
+              <h2 id="download-heading" className="download-heading font-bold align-center m-10">
+                写真をクリックすると、ダウンロードが始まります。
+              </h2>
+              <div id="screenshot-div" ref={screenshotRef} className="screenshot-div flex flex-col items-center">
+                <Image
+                  className="template-base"
+                  id="template-base"
+                  width={984}
+                  height={1387}
+                  alt="not found"
+                  src={template.src}
+                />
+
+                <Image
+                  className="picture"
+                  id="picture"
+                  width={646}
+                  height={424.5}
+                  alt="not found"
+                  src={URL.createObjectURL(submittedImage)}
+                />
+
+                <p className="poster-oshikatsu" id="poster-oshikatsu">{submittedText.oshikatsu}</p>
+                <p className="poster-mirai" id="poster-mirai">{submittedText.mirai}</p>
+                <p className="poster-name-age" id="poster-name-age">{submittedText.penname}・{submittedText.age}歳</p>
+                <a id="download-div" className="download-div w-full h-full" onClick={screenshotDownload}></a>
+              </div>
+              <button
+                id="download-button"
+                className="download-button rounded-md bg-indigo-600 my-10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={screenshotDownload}
+              >
+                保存する
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
 
   );
