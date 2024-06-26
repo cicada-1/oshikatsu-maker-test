6d91e495a89f3cf2f88c703c23d3e4baeafa823a
Peter Wallace <peterwallace@hotmail.co.uk>
2024-06-26 15:50:19 +0900

    Removed English versions

--- app/components/explanation.tsx
+++ app/components/explanation.tsx
@@ -1,34 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
 import posterExampleJp from '../assets/images/poster-example-jp.jpg';
-import posterExampleEn from '../assets/images/poster-example-en.jpg';
 
-export default function JapaneseExplanation(props: any) {
-  const explanationText = {
-    Japanese: {
-      title: '私の「推し勝★」ジェネレーター',
-      body1: `自分の「推し勝★」を創ってみましょう！\n\n私の「推し勝★」ジェネレーターに：\n\n-「推し勝★」\n- 創る未来\n- ペンネーム\n- 年齢\n- 写真\n\nを入力して、キャンペーンのポスターのように自分のができます！`,
-      imageNote: 'ポスターの例',
-      body2: `このように完成すると、保存できますので、完成したポスターか、「保存する」のボタンをクリックしてください！\nポスターを保存してから、「#私の推し勝」「#勝山しか勝たん山」のハッシュタグでSNSに投稿しましょう！\nでは、私の「推し勝★」ジェネレーターで楽しみましょう！`,
-    },
-    English: {
-      title: 'My "Oshi-Katsu" Generator!',
-      body1: `Create your own "Oshi-Katsu" poster!\n\nPlease enter your:\n\n- "Oshi-Katsu"\n- belief for the future\n- nickname\n- age\n- picture\n\nto the generator and you can make your own poster, just like those of the official campaign!`,
-      imageNote: 'Example poster',
-      body2: `Once your poster has been generated, click on the generated poster or the "Download" button below to download your poster.\nIf you upload your poster to social media, remember to use the hashtags #私の推し勝, #勝山しか勝たん山. Or in English #MyOshiKatsu, #KatsuyamaShikaKatanYama.\nHave fun with the My "Oshi-Katsu" Generator!`,
-    },
-  };
-
-  var explanation = props.setLanguage ? explanationText.Japanese : explanationText.English;
+export default function Explanation() {
 
   return (
     <div className="heading m-5 flex flex-col items-center">
-      <h1 className="form-title mb-5 font-bold leading-7 text-gray-900">{explanation.title}</h1>
+      <h1 className="form-title mb-5 font-bold leading-7 text-gray-900">私の「推し勝★」ジェネレーター</h1>
       <p
         className="explanation"
         style={{ whiteSpace: 'pre-wrap' }}
       >
-        {explanation.body1}<br /> <br />
+        {`自分の「推し勝★」を創ってみましょう！\n\n私の「推し勝★」ジェネレーターに：\n\n-「推し勝★」\n- 創る未来\n- ペンネーム\n- 年齢\n- 写真\n\nを入力して、キャンペーンのポスターのように自分のができます！`}<br /> <br />
       </p>
       <div className="my-5 flex flex-col items-center">
         <Image
@@ -36,12 +19,12 @@ export default function JapaneseExplanation(props: any) {
           width={300}
           height={243}
           alt="not found"
-          src={props.setLanguage ? posterExampleJp.src : posterExampleEn.src}
+          src={posterExampleJp.src}
         />
-        <p className="image-note"><em>{explanation.imageNote}</em></p>
+        <p className="image-note"><em>ポスターの例</em></p>
       </div>
       <p className="explanation">
-        {explanation.body2}
+        {`このように完成すると、保存できますので、完成したポスターか、「保存する」のボタンをクリックしてください！\nポスターを保存してから、「#私の推し勝」「#勝山しか勝たん山」のハッシュタグでSNSに投稿しましょう！\nでは、私の「推し勝★」ジェネレーターで楽しみましょう！`}
       </p>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,33 +2,20 @@
 'use client';
 
 import React from 'react';
-import { useState } from 'react';
 
 import Banner from './components/banner';
 import Explanation from './components/explanation';
 import Form from './components/form';
-import LanguageButtons from './components/language-buttons';
 
 export default function Home() {
-
-  const [isJapanese, setIsJapanese] = useState(true);
 
   return (
     <main className="flex min-h-screen flex-col items-center">
       <Banner />
 
-      <LanguageButtons
-        setLanguage={isJapanese}
-        onChange={() => setIsJapanese(!isJapanese)}
-      />
+      <Explanation />
 
-      <Explanation
-        setLanguage={isJapanese}
-      />
-
-      <Form
-        setLanguage={isJapanese}
-      />
+      <Form />
     </main>
   );
 }
