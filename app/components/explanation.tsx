c9052ef4e3bff257eeecc852367261538c3abc14
Peter Wallace <peterwallace@hotmail.co.uk>
2024-06-28 15:45:27 +0900

    Changed site name to Oshikatsu Maker

--- app/components/explanation.tsx
+++ app/components/explanation.tsx
@@ -6,12 +6,12 @@ export default function Explanation() {
 
   return (
     <div className="heading m-5 flex flex-col items-center">
-      <h1 className="form-title mb-5 font-bold leading-7 text-gray-900">私の「推し勝★」ジェネレーター</h1>
+      <h1 className="form-title mb-5 font-bold leading-7 text-gray-900">私の「推し勝★」メーカー</h1>
       <p
         className="explanation"
         style={{ whiteSpace: 'pre-wrap' }}
       >
-        {`自分の「推し勝★」を創ってみましょう！\n\n私の「推し勝★」ジェネレーターに：\n\n-「推し勝★」\n- 創る未来\n- ペンネーム\n- 年齢\n- 写真\n\nを入力して、キャンペーンのポスターのように自分のができます！`}<br /> <br />
+        {`自分の「推し勝★」を創ってみましょう！\n\n私の「推し勝★」メーカーに：\n\n-「推し勝★」\n- 創る未来\n- ペンネーム\n- 年齢\n- 写真\n\nを入力して、自分の「推し勝★」ポスターが創れます！`}<br /> <br />
       </p>
       <div className="my-5 flex flex-col items-center">
         <Image
@@ -24,7 +24,7 @@ export default function Explanation() {
         <p className="image-note"><em>ポスターの例</em></p>
       </div>
       <p className="explanation">
-        {`このように完成すると、保存できますので、完成したポスターか、「保存する」のボタンをクリックしてください！\nポスターを保存してから、「#私の推し勝」「#勝山しか勝たん山」のハッシュタグでSNSに投稿しましょう！\nでは、私の「推し勝★」ジェネレーターで楽しみましょう！`}
+        {`このように完成すると、保存できますので、完成したポスターか、「保存する」のボタンをクリックしてください！\nポスターを保存してから、「#私の推し勝」「#勝山しか勝たん山」のハッシュタグでSNSに投稿しましょう！\nでは、私の「推し勝★」メーカーで楽しみましょう！`}
       </p>
     </div>
   );
