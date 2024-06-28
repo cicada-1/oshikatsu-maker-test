d962ef78ce177817fb75ff0aa544c56b6cd5d440
Peter Wallace <peterwallace@hotmail.co.uk>
2024-06-28 16:06:18 +0900

    Added iphone screenshot information

--- app/components/explanation.tsx
+++ app/components/explanation.tsx
@@ -7,10 +7,7 @@ export default function Explanation() {
   return (
     <div className="heading m-5 flex flex-col items-center">
       <h1 className="form-title mb-5 font-bold leading-7 text-gray-900">私の「推し勝★」メーカー</h1>
-      <p
-        className="explanation"
-        style={{ whiteSpace: 'pre-wrap' }}
-      >
+      <p className="explanation">
         {`自分の「推し勝★」を創ってみましょう！\n\n私の「推し勝★」メーカーに：\n\n-「推し勝★」\n- 創る未来\n- ペンネーム\n- 年齢\n- 写真\n\nを入力して、自分の「推し勝★」ポスターが創れます！`}<br /> <br />
       </p>
       <div className="my-5 flex flex-col items-center">
@@ -24,7 +21,10 @@ export default function Explanation() {
         <p className="image-note"><em>ポスターの例</em></p>
       </div>
       <p className="explanation">
-        {`このように完成すると、保存できますので、完成したポスターか、「保存する」のボタンをクリックしてください！\nポスターを保存してから、「#私の推し勝」「#勝山しか勝たん山」のハッシュタグでSNSに投稿しましょう！\nでは、私の「推し勝★」メーカーで楽しみましょう！`}
+        {`このように完成すると、保存できますので、完成したポスターか、「保存する」のボタンをクリックしてください！*\nポスターを保存してから、「#私の推し勝」「#勝山しか勝たん山」のハッシュタグでSNSに投稿しましょう！\nでは、私の「推し勝★」メーカーで楽しみましょう！\n\n`}
+      </p>
+      <p className="explanation-note">
+        {`*iPhoneで保存機能が動きませんので、iPhoneの方がスクリーンショットを撮ってください。\n"音量を減らす"ボタンと"画面をロックする"ボタンを同時に押すと、スクリーンショットが撮れます。\nご迷惑をかけして申し訳ありません。`}
       </p>
     </div>
   );
