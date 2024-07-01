36f6d2bf34edc9bcdf20fa4789a5bda888449319
Peter Wallace <peterwallace@hotmail.co.uk>
2024-07-01 11:48:07 +0900

    Added language buttons with isJapanese state

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,24 @@
 'use client';
 
 import React from 'react';
-
+import { useState } from 'react';
 import Banner from './components/banner';
+import LanguageButtons from './components/language-buttons';
 import Explanation from './components/explanation';
 import Form from './components/form';
 
 export default function Home() {
 
+  const [isJapanese, setIsJapanese] = useState(true);
+
   return (
     <main className="flex min-h-screen flex-col items-center">
       <Banner />
+
+      <LanguageButtons
+        setLanguage={isJapanese}
+        onChange={() => setIsJapanese(!isJapanese)}
+      />
 
       <Explanation />
 
