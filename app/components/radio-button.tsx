cdbc63c995d64ad80bb112cffc9a7f3cab2b2b16
Peter Wallace <peterwallace@hotmail.co.uk>
2024-07-05 11:21:01 +0900

    Made radio button into a component and iterated through template design object to dynamically generate the design options

---
+++ app/components/radio-button.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+
+export default function RadioButton(props: any) {
+
+  return (
+    <label>
+      <input
+        type="radio"
+        name="design"
+        value={props.value}
+        required={true}
+        className="design-choice hidden"
+        onChange={props.onChange}
+      />
+      <img
+        className="design-choice-image"
+        src={props.src}
+        alt="not found"
+      />
+    </label>
+  );
+}
